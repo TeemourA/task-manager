06f602db4eb0c529f9a9653a2ee39032c4d443c3
Timur Akhmedov <tahmedov@dev.vtb.ru>
2022-01-11 12:31:16 +0300

    disable cors restriction

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express, { request } from 'express';
+import express from 'express';
 import bcrypt from 'bcryptjs';
 
 import { dbConnect } from './db/mongoose.js';
@@ -10,22 +10,19 @@ dbConnect();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+
+  next();
+});
 app.use(express.json());
 app.use([userRouter, taskRouter]);
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
-
-// const func = async () => {
-//   const pass = 'Red12345!';
-//   const hashedPass = await bcrypt.hash(pass, bcrypt.genSaltSync(8));
-//   const isMatch = await bcrypt.compare(pass, hashedPass);
-//   console.log(pass);
-//   console.log(hashedPass);
-//   console.log(isMatch);
-
-//   return hashedPass;
-// };
-
-// func();
