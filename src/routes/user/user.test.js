5c585413309857d6135c0f9a66972d2d5eff6199
TeemourA <taxprogramming@gmail.com>
2022-03-10 22:10:11 +0300

    extend user route tests

--- src/routes/user/user.test.js
+++ src/routes/user/user.test.js
@@ -39,17 +39,37 @@ beforeEach(async () => {
 });
 
 test('Should signup a new user', async () => {
-  await request(app).post('/users').send(user1).expect(201);
+  const res = await request(app).post('/users').send(user1).expect(201);
+
+  // Assert that the new user was added correctly
+  const user = await User.findById(res.body.user._id);
+  expect(user).not.toBe(null);
+
+  // Assertions about the res
+  expect(res.body).toMatchObject({
+    user: {
+      name: user1.name,
+      email: user1.email,
+    },
+    token: user.tokens[0].token,
+  });
+
+  // Assert that the user password is not a plain text
+  expect(user.password).not.toBe(user1.password);
 });
 
 test('Should login existing user', async () => {
-  await request(app)
+  const res = await request(app)
     .post('/users/login')
     .send({
       email: user2.email,
       password: user2.password,
     })
     .expect(200);
+
+  // Assert that the new token was added to the user's token list
+  const user = await User.findById(res.body.user._id);
+  expect(user.tokens[user.tokens.length - 1].token).toBe(res.body.token);
 });
 
 test('Should not login nonexisting user', async () => {
@@ -75,11 +95,15 @@ test('Should not get profile for unauthenticated user', async () => {
 });
 
 test('Should delete profile for authenticated user', async () => {
-  await request(app)
+  const res = await request(app)
     .delete('/users/me')
     .set(authorization, `Bearer ${user2.tokens[0].token}`)
     .send()
     .expect(200);
+
+  // Assert that the user is succesfully deleted from database
+  const user = await User.findById(res.body._id);
+  expect(user).toBeNull();
 });
 
 test('Should not delete profile for unauthenticated user', async () => {
